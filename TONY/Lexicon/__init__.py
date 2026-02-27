0bdd6c98b4ac4f6dd792a3e40232a971f5d69118
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2026-02-27 11:26:33 +0100

    Update __init__.py

--- TONY/Lexicon/__init__.py
+++ TONY/Lexicon/__init__.py
@@ -1,6 +1,6 @@
 # TONY/Lexicon/__init__.py
 from .LinguisticMarkers import *  
-
+from .APP_Colab import *
 try:
     from .APP import *  
 except ImportError:
@@ -8,9 +8,3 @@ except ImportError:
 except Exception:
     pass  # tkinter può lanciare anche TclError se non c'è display
 
-try:
-    from .APP_Colab import *  
-except ImportError:
-    pass
-except Exception:
-    pass  
